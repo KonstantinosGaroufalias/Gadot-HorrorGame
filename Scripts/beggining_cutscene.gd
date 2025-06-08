7fa63746477a10f8eb6a7f1400b00a7ad7767d88
KonstantinosGaroufalias <mastershadowgr3@gmail.com>
2025-06-08 18:20:45 +0300

    Animation

--- Scripts/beggining_cutscene.gd
+++ Scripts/beggining_cutscene.gd
@@ -1,11 +1,10 @@
 extends Node3D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$cutscene_anim_player.play("beggining")
+	$cutscene_anim_player.play("beggining_drive")
 	$cutscene_cam.current = true
-	await get_tree().create_timer(6.1,false).timeout
+	await get_tree().create_timer(15,false).timeout
 	get_node("/root/"+get_tree().current_scene.name+"/Player").movable = true
 	get_node("/root/"+get_tree().current_scene.name+"/Player/head/Camera3D").current = true
 	get_node("/root/"+get_tree().current_scene.name+"/Player/head").movable = true
